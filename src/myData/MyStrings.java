26cd73b70e0daadb6080cc2c73dc1d9223ae541f
Owen <owen2284@googlemail.com>
2016-07-15 16:48:20 +0100

    Added new methods for string actions.

--- src/myData/MyStrings.java
+++ src/myData/MyStrings.java
@@ -3,7 +3,7 @@ package myData;
 public class MyStrings {
 	
 	// Function for buffering a string to a certain length.
-	public String bufferString(String initString, int targetLength, String bufferString) {
+	public static String bufferString(String initString, int targetLength, String bufferString) {
 		if (bufferString.length() >= 1) {
 			String finalString = initString;
 			while (finalString.length() < targetLength) {finalString += bufferString;}
@@ -11,6 +11,12 @@ public class MyStrings {
 		} else {
 			return initString;
 		}
+	}
+	
+	// Function for advancing one item through a sequence of strings.
+	public static String sequenceAdvance(String current, String[] sequence, boolean forwards) {
+		// TODO: Complete sequence advance function.
+		return "";
 	}
 
 }
